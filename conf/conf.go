2e5dea2b0b5da0a86b25a4c3405b6d1fe4691bc3
Stephane Martin <stephane.martin@vesperal.eu>
2017-04-30 17:03:50 +0200

    recover from viper

--- conf/conf.go
+++ conf/conf.go
@@ -3,24 +3,25 @@ package conf
 import (
 	"bytes"
 	"crypto/rand"
+	"errors"
 	"fmt"
-	"strings"
 	"strconv"
+	"strings"
 
 	"github.com/BurntSushi/toml"
 	"github.com/go-redis/redis"
 	"github.com/hashicorp/errwrap"
 	"github.com/spf13/viper"
-	"github.com/stephane-martin/nginx-auth-ldap/log"
 	"github.com/stephane-martin/nginx-auth-ldap/consul"
+	"github.com/stephane-martin/nginx-auth-ldap/log"
 )
 
 type GlobalConfig struct {
-	Ldap  []LdapConfig  `mapstructure:"ldap" toml:"ldap"`
-	Http  HttpConfig  `mapstructure:"http" toml:"http"`
-	Api   ApiConfig   `mapstructure:"api" toml:"api"`
-	Cache CacheConfig `mapstructure:"cache" toml:"cache"`
-	Redis RedisConfig `mapstructure:"redis" toml:"redis"`
+	Ldap  []LdapConfig `mapstructure:"ldap" toml:"ldap"`
+	Http  HttpConfig   `mapstructure:"http" toml:"http"`
+	Api   ApiConfig    `mapstructure:"api" toml:"api"`
+	Cache CacheConfig  `mapstructure:"cache" toml:"cache"`
+	Redis RedisConfig  `mapstructure:"redis" toml:"redis"`
 }
 
 type LdapConfig struct {
@@ -40,19 +41,19 @@ type LdapConfig struct {
 }
 
 var DefaultLdapConfig LdapConfig = LdapConfig{
-	Host: "127.0.0.1",
-	Port: 389,
-	AuthType: "directbind",
-	BindDn: "",
-	BindPassword: "",
+	Host:             "127.0.0.1",
+	Port:             389,
+	AuthType:         "directbind",
+	BindDn:           "",
+	BindPassword:     "",
 	UserSearchFilter: "(uid=%s)",
-	UserSearchBase: "ou=users,dc=example,dc=org",
-	UserDnTemplate: "uid=%s,ou=users,dc=example,dc=org",
-	TlsType: "none",
-	CA: "",
-	Cert: "",
-	Key: "",
-	Insecure: false,
+	UserSearchBase:   "ou=users,dc=example,dc=org",
+	UserDnTemplate:   "uid=%s,ou=users,dc=example,dc=org",
+	TlsType:          "none",
+	CA:               "",
+	Cert:             "",
+	Key:              "",
+	Insecure:         false,
 }
 
 type ApiConfig struct {
@@ -145,7 +146,6 @@ func (c *GlobalConfig) GenerateSecret() (secret []byte, err error) {
 
 func (c *GlobalConfig) Check() error {
 
-	
 	for _, l := range c.Ldap {
 		if l.Port == 0 {
 			return fmt.Errorf("LDAP port can't be 0")
@@ -181,7 +181,7 @@ func (c *GlobalConfig) Check() error {
 			if !l.Insecure && len(l.CA) == 0 {
 				return fmt.Errorf("Specify the certificate authority 'ldap.certificate_authority' that is used to verify the LDAP server certificate")
 			}
-		}	
+		}
 	}
 
 	if c.Http.Port == 0 {
@@ -207,10 +207,30 @@ func (c *GlobalConfig) Check() error {
 	return nil
 }
 
-func Load(dirname, consul_addr, consul_prefix, consul_token, consul_datacenter string, consul_notify chan bool) (*GlobalConfig, chan bool, error) {
+func Load(dirname, c_addr, c_prefix, c_token, c_dtctr string, consul_notify chan bool) (conf *GlobalConfig, stop_chan chan bool, err error) {
+	defer func() {
+		// sometimes viper panics
+		if r := recover(); r != nil {
+			log.Log.WithField("recover", r).Error("Recovered in conf.Load")
+			// find out exactly what the error was and set err
+			switch x := r.(type) {
+			case string:
+				err = errors.New(x)
+			case error:
+				err = x
+			default:
+				err = errors.New("Unknown panic")
+			}
+			// invalidate other returns
+			conf = nil
+			stop_chan = nil
+		}
+
+	}()
+
 	// we should close consul_notify in all cases
 	v := viper.New()
-	v.SetDefault("ldap", []LdapConfig{DefaultLdapConfig})
+	//v.SetDefault("ldap", []LdapConfig{DefaultLdapConfig})
 
 	v.SetDefault("http.bind_addr", "0.0.0.0")
 	v.SetDefault("http.port", 8080)
@@ -242,19 +262,19 @@ func Load(dirname, consul_addr, consul_prefix, consul_token, consul_datacenter s
 	v.SetDefault("redis.expires_seconds", 86400)
 
 	/*
-	v.BindEnv("ldap.host", "NAL_LDAP_HOST")
-	v.BindEnv("ldap.port", "NAL_LDAP_PORT")
-	v.BindEnv("ldap.auth_type", "NAL_AUTH_TYPE")
-	v.BindEnv("ldap.bind_dn", "NAL_BIND_DN")
-	v.BindEnv("ldap.bind_password", "NAL_BIND_PASSWORD")
-	v.BindEnv("ldap.user_search_filter", "NAL_SEARCH_FILTER")
-	v.BindEnv("ldap.user_search_base", "NAL_SEARCH_BASE")
-	v.BindEnv("ldap.user_dn_template", "NAL_USER_TEMPLATE")
-	v.BindEnv("ldap.tls_type", "NAL_LDAP_TLS")
-	v.BindEnv("ldap.certificate_authority", "NAL_LDAP_CA")
-	v.BindEnv("ldap.certificate", "NAL_LDAP_CERT")
-	v.BindEnv("ldap.key", "NAL_LDAP_KEY")
-	v.BindEnv("ldap.insecure", "NAL_LDAP_INSECURE")
+		v.BindEnv("ldap.host", "NAL_LDAP_HOST")
+		v.BindEnv("ldap.port", "NAL_LDAP_PORT")
+		v.BindEnv("ldap.auth_type", "NAL_AUTH_TYPE")
+		v.BindEnv("ldap.bind_dn", "NAL_BIND_DN")
+		v.BindEnv("ldap.bind_password", "NAL_BIND_PASSWORD")
+		v.BindEnv("ldap.user_search_filter", "NAL_SEARCH_FILTER")
+		v.BindEnv("ldap.user_search_base", "NAL_SEARCH_BASE")
+		v.BindEnv("ldap.user_dn_template", "NAL_USER_TEMPLATE")
+		v.BindEnv("ldap.tls_type", "NAL_LDAP_TLS")
+		v.BindEnv("ldap.certificate_authority", "NAL_LDAP_CA")
+		v.BindEnv("ldap.certificate", "NAL_LDAP_CERT")
+		v.BindEnv("ldap.key", "NAL_LDAP_KEY")
+		v.BindEnv("ldap.insecure", "NAL_LDAP_INSECURE")
 	*/
 
 	v.BindEnv("http.bind_addr", "NAL_HTTP_ADDR")
@@ -296,7 +316,7 @@ func Load(dirname, consul_addr, consul_prefix, consul_token, consul_datacenter s
 		v.AddConfigPath("/etc")
 	}
 
-	err := v.ReadInConfig()
+	err = v.ReadInConfig()
 	if err == nil {
 		log.Log.WithField("file", v.ConfigFileUsed()).Debug("Found configuration file")
 	} else {
@@ -311,11 +331,10 @@ func Load(dirname, consul_addr, consul_prefix, consul_token, consul_datacenter s
 		}
 	}
 
-	var stop_chan chan bool
 	var config_in_consul map[string]string
 
-	if len(consul_addr) > 0 {
-		consul_client, err := consul.NewClient(consul_addr, consul_token, consul_datacenter)
+	if len(c_addr) > 0 {
+		consul_client, err := consul.NewClient(c_addr, c_token, c_dtctr)
 		if err != nil {
 			if consul_notify != nil {
 				close(consul_notify)
@@ -323,22 +342,22 @@ func Load(dirname, consul_addr, consul_prefix, consul_token, consul_datacenter s
 			return nil, nil, err
 		}
 		if consul_notify == nil {
-			config_in_consul, _, err = consul.WatchTree(consul_client, consul_prefix, nil) 
+			config_in_consul, _, err = consul.WatchTree(consul_client, c_prefix, nil)
 		} else {
 			// responsability to close consul_notify is transfered to WatchTree
-			config_in_consul, stop_chan, err = consul.WatchTree(consul_client, consul_prefix, consul_notify) 
+			config_in_consul, stop_chan, err = consul.WatchTree(consul_client, c_prefix, consul_notify)
 		}
 		if err != nil {
 			return nil, nil, err
 		}
-		ParseConfigFromConsul(v, consul_prefix, config_in_consul)
+		ParseConfigFromConsul(v, c_prefix, config_in_consul)
 	} else {
 		if consul_notify != nil {
 			close(consul_notify)
 		}
 	}
 
-	conf := New()
+	conf = New()
 	err = v.Unmarshal(conf)
 	if err != nil {
 		if stop_chan != nil {
@@ -346,12 +365,10 @@ func Load(dirname, consul_addr, consul_prefix, consul_token, consul_datacenter s
 		}
 		return nil, nil, errwrap.Wrapf("Error parsing configuration", err)
 	}
-	
+
 	if len(config_in_consul) > 0 {
-		ParseLdapConfigFromConsul(conf, consul_prefix, config_in_consul)
+		ParseLdapConfigFromConsul(conf, c_prefix, config_in_consul)
 	}
-
-
 
 	// inject defaults into LDAP configurations
 	for i, _ := range conf.Ldap {
@@ -422,59 +439,75 @@ func ParseLdapConfigFromConsul(conf *GlobalConfig, prefix string, c map[string]s
 	for bucket, m := range ldap_configs {
 		ldap_config := LdapConfig{}
 		for k, v := range m {
-			log.Log.WithField("ldap_id", bucket).WithField(k, v).Debug("LDAP configuration from Consul")
 			switch k {
 			case "host":
 				ldap_config.Host = v
+				log.Log.WithField("ldap_id", bucket).WithField(k, v).Debug("LDAP configuration from Consul")
 			case "port":
 				port, err := strconv.ParseInt(v, 10, 32)
 				if err == nil {
 					ldap_config.Port = uint32(port)
+					log.Log.WithField("ldap_id", bucket).WithField(k, v).Debug("LDAP configuration from Consul")
+				} else {
+					log.Log.WithError(err).WithField(k, v).Warn("LDAP port in consul has wrong format. Ignoring")
 				}
 			case "auth_type":
 				ldap_config.AuthType = v
+				log.Log.WithField("ldap_id", bucket).WithField(k, v).Debug("LDAP configuration from Consul")
 			case "bind_dn":
 				ldap_config.BindDn = v
+				log.Log.WithField("ldap_id", bucket).WithField(k, v).Debug("LDAP configuration from Consul")
 			case "bind_password":
 				ldap_config.BindPassword = v
+				log.Log.WithField("ldap_id", bucket).WithField(k, v).Debug("LDAP configuration from Consul")
 			case "user_search_filter":
 				ldap_config.UserSearchFilter = v
+				log.Log.WithField("ldap_id", bucket).WithField(k, v).Debug("LDAP configuration from Consul")
 			case "user_search_base":
 				ldap_config.UserSearchBase = v
+				log.Log.WithField("ldap_id", bucket).WithField(k, v).Debug("LDAP configuration from Consul")
 			case "user_dn_template":
 				ldap_config.UserDnTemplate = v
+				log.Log.WithField("ldap_id", bucket).WithField(k, v).Debug("LDAP configuration from Consul")
 			case "tls_type":
 				ldap_config.TlsType = v
+				log.Log.WithField("ldap_id", bucket).WithField(k, v).Debug("LDAP configuration from Consul")
 			case "certificate_authority":
 				ldap_config.CA = v
+				log.Log.WithField("ldap_id", bucket).WithField(k, v).Debug("LDAP configuration from Consul")
 			case "certificate":
 				ldap_config.Cert = v
+				log.Log.WithField("ldap_id", bucket).WithField(k, v).Debug("LDAP configuration from Consul")
 			case "key":
 				ldap_config.Key = v
+				log.Log.WithField("ldap_id", bucket).WithField(k, v).Debug("LDAP configuration from Consul")
 			case "insecure":
 				insecure, err := strconv.ParseBool(v)
 				if err == nil {
 					ldap_config.Insecure = insecure
+					log.Log.WithField("ldap_id", bucket).WithField(k, v).Debug("LDAP configuration from Consul")
+				} else {
+					log.Log.WithError(err).WithField(k, v).Warn("LDAP insecure parameter in consul has wrong format. Ignoring")
 				}
 			default:
+				log.Log.WithField(k, v).Warn("Ignoring LDAP parameter from Consul")
 			}
 		}
 		conf.Ldap = append(conf.Ldap, ldap_config)
 	}
 	/*
-	v.SetDefault("ldap.host", "127.0.0.1")
-	v.SetDefault("ldap.port", 389)
-	v.SetDefault("ldap.auth_type", "directbind")
-	v.SetDefault("ldap.bind_dn", "")
-	v.SetDefault("ldap.bind_password", "")
-	v.SetDefault("ldap.user_search_filter", "(uid=%s)")
-	v.SetDefault("ldap.user_search_base", "ou=users,dc=example,dc=org")
-	v.SetDefault("ldap.user_dn_template", "uid=%s,ou=users,dc=example,dc=org")
-	v.SetDefault("ldap.tls_type", "none")
-	v.SetDefault("ldap.certificate_authority", "")
-	v.SetDefault("ldap.certificate", "")
-	v.SetDefault("ldap.key", "")
-	v.SetDefault("ldap.insecure", false)
+		v.SetDefault("ldap.host", "127.0.0.1")
+		v.SetDefault("ldap.port", 389)
+		v.SetDefault("ldap.auth_type", "directbind")
+		v.SetDefault("ldap.bind_dn", "")
+		v.SetDefault("ldap.bind_password", "")
+		v.SetDefault("ldap.user_search_filter", "(uid=%s)")
+		v.SetDefault("ldap.user_search_base", "ou=users,dc=example,dc=org")
+		v.SetDefault("ldap.user_dn_template", "uid=%s,ou=users,dc=example,dc=org")
+		v.SetDefault("ldap.tls_type", "none")
+		v.SetDefault("ldap.certificate_authority", "")
+		v.SetDefault("ldap.certificate", "")
+		v.SetDefault("ldap.key", "")
+		v.SetDefault("ldap.insecure", false)
 	*/
 }
-
