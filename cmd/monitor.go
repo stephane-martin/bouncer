9dbfb043699c57a6a6f57096b21abc060a071d36
Stephane Martin <stephane.martin@vesperal.eu>
2017-05-01 19:35:39 +0200

    the monitor command can also display previous requests logs through --back

--- cmd/monitor.go
+++ cmd/monitor.go
@@ -1,14 +1,20 @@
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
 	"os/signal"
+	"sort"
+	"strconv"
 	"syscall"
+	"time"
 
+	"github.com/go-redis/redis"
 	"github.com/spf13/cobra"
 	"github.com/stephane-martin/nginx-auth-ldap/conf"
 	"github.com/stephane-martin/nginx-auth-ldap/log"
+	"github.com/stephane-martin/nginx-auth-ldap/model"
 	"github.com/stephane-martin/nginx-auth-ldap/stats"
 )
 
@@ -23,11 +29,11 @@ as they come.`,
 	},
 }
 
-var back_period uint64
+var back_period int64
 
 func init() {
 	RootCmd.AddCommand(monitorCmd)
-	monitorCmd.Flags().Uint64Var(&back_period, "back", 60, "Print the requests that happened in the last number of seconds")
+	monitorCmd.Flags().Int64Var(&back_period, "back", 0, "Also print the requests that happened in the last number of seconds")
 }
 
 func monitor() {
@@ -54,7 +60,43 @@ func monitor() {
 	client := config.GetRedisClient()
 
 	if back_period > 0 {
-
+		now := time.Now().UnixNano()
+		from := now - (back_period * 1000000000)
+		r := redis.ZRangeBy{Min: strconv.FormatInt(from, 10), Max: strconv.FormatInt(now, 10)}
+		pipe := client.TxPipeline()
+		cmds := []*redis.StringSliceCmd{}
+		for _, t := range model.ResultTypes {
+			sset := fmt.Sprintf(stats.SET_REQUESTS_TPL, t)
+			cmds = append(cmds, pipe.ZRangeByScore(sset, r))
+		}
+		_, err := pipe.Exec()
+		if err != nil {
+			log.Log.WithError(err).Error("Error getting logs from Redis")
+			os.Exit(-1)
+		}
+		events := model.PackOfEvents{}
+		for _, cmd := range cmds {
+			for _, event_s := range cmd.Val() {
+				ev := model.Event{}
+				err := json.Unmarshal([]byte(event_s), &ev)
+				if err == nil {
+					events = append(events, &ev)
+				} else {
+					log.Log.WithError(err).WithField("event", event_s).Warn("Error decoding an event from Redis")
+				}
+			}
+		}
+		sort.Sort(events)
+		for _, ev := range events {
+			if Json {
+				b, err := json.Marshal(*ev)
+				if err == nil {
+					fmt.Println(string(b))
+				}
+			} else {
+				fmt.Println(ev)	
+			}
+		}
 	}
 	pubsub := client.Subscribe(stats.NOTIFICATIONS_REDIS_CHAN)
 	defer pubsub.Close()
@@ -74,7 +116,15 @@ func monitor() {
 			restart = false
 		case msg, more := <-msg_chan:
 			if more {
-				fmt.Println(msg.Payload)
+				ev := model.Event{}
+				err := json.Unmarshal([]byte(msg.Payload), &ev)
+				if err != nil {
+					log.Log.WithError(err).WithField("event", msg.Payload).Warn("Error decoding an event from Redis")
+				} else if Json {
+					fmt.Println(msg.Payload)
+				} else {
+					fmt.Println(&ev)
+				}
 			} else {
 				restart = false
 			}
--- model/model.go
+++ model/model.go
@@ -3,10 +3,12 @@ package model
 import (
 	"crypto/hmac"
 	"crypto/sha256"
+	"fmt"
 	"time"
 )
 
 type CounterID uint8
+
 const (
 	LDAP_CONN_ERROR = iota
 	RESTARTS
@@ -18,16 +20,17 @@ const (
 )
 
 var CounterNames map[CounterID]string = map[CounterID]string{
-	LDAP_CONN_ERROR: "ldap_connection_error",
-	RESTARTS: "restarts",
+	LDAP_CONN_ERROR:  "ldap_connection_error",
+	RESTARTS:         "restarts",
 	HTTP_ABRUPT_TERM: "http_abrupt_termination",
-	API_ABRUPT_TERM: "api_abrupt_termination",
-	SIGTERM_SIGINT: "sigterm_sigint_received",
-	SIGHUP: "sighup_received",
-	UNKNOWN_SIG: "unknown_signal_received",
+	API_ABRUPT_TERM:  "api_abrupt_termination",
+	SIGTERM_SIGINT:   "sigterm_sigint_received",
+	SIGHUP:           "sighup_received",
+	UNKNOWN_SIG:      "unknown_signal_received",
 }
 
 type Result uint8
+
 const (
 	SUCCESS_AUTH Result = iota
 	SUCCESS_AUTH_CACHE
@@ -50,6 +53,33 @@ type Event struct {
 	Timestamp int64  `json:"timestamp,string"`
 	Message   string `json:"message"`
 	Result    Result `json:"result"`
+}
+
+const EventStringFormat string = `Timestamp %d
+Proto: %s
+Host: %s
+Port: %s
+Uri: %s
+Username: %s
+Returned code: %d
+Result: %d
+Message: %s
+`
+
+func (e *Event) String() string {
+	return fmt.Sprintf(EventStringFormat, e.Timestamp, e.Proto, e.Host, e.Port, e.Uri, e.Username, e.RetCode, e.Result, e.Message)
+}
+
+type PackOfEvents []*Event
+
+func (p PackOfEvents) Len() int {
+	return len(p)
+}
+func (p PackOfEvents) Swap(i, j int) {
+	p[i], p[j] = p[j], p[i]
+}
+func (p PackOfEvents) Less(i, j int) bool {
+	return p[i].Timestamp < p[j].Timestamp
 }
 
 func NewEmptyEvent() *Event {
